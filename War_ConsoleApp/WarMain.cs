075b3cef5acd38ae9156990cf4431411b153f3d1
Erock <millerea1983@protonmail.com>
2021-11-02 21:41:09 -0400

    Trying to verify the output file directory for game recap/metrics and starting game logic.
    EAM

--- War_ConsoleApp/WarMain.cs
+++ War_ConsoleApp/WarMain.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.IO;
+
 namespace War_ConsoleApp
 {
     class WarMain
     {
         static void Main(string[] args)
         {
-            Console.Write("Begin War Main...\n");
+            string root = Directory.GetCurrentDirectory();
+            string subdir = root + "\\Output\\";
+            // If directory does not exist, create it. 
+            if (!Directory.Exists(root))
+            {
+                Directory.CreateDirectory(subdir);
+            }
+            if(!Directory.Exists(subdir))
+            {
+                Directory.CreateDirectory(subdir);
+                //Console.WriteLine("Directory created: " + subdir);
+            }
 
             //TODO define controller / game play
             //TODO - how to deal
@@ -18,8 +31,8 @@ namespace War_ConsoleApp
             //TODO what else are you missing?
 
             WarGame game = new WarGame();
-            //bool val = game.PromptWarGame(); //TODO put this back in for prompting
-            bool val = true;
+            bool val = game.PromptWarGame(); //TODO put this back in for prompting
+            //bool val = true;
             if (val == true)//TODO put back in for prompting
             {
                 game.NewGame();
@@ -28,7 +41,6 @@ namespace War_ConsoleApp
             {
                 //No game END / ESC
             }
-
 
             Console.Read();
         }
